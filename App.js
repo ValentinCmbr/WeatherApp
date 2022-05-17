d083a95a1900c4755f0548de520acb45197fb58f
ValentinCmbr <v.combier@lyon.ort.asso.fr>
2022-05-17 15:23:00 +0200

    Appel de l'API | Affichage de la ville, temperature, description
    Appel de l'API | Affichage de la ville, temperature, description

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Platform, Text, View, StyleSheet, Image } from 'react-native';
 import * as Location from 'expo-location';
 
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [ville, setVille] = useState(null);
+  const [temperature, setTemperature] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [icone, setIcone] = useState(null);
+
+  let key = '54b2306084d05aace9fbc9b7a49254fc';
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        setErrorMsg('Permission non accordée');
         return;
       }
 
@@ -19,16 +25,29 @@ export default function App() {
     })();
   }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+  const MeteoVille = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${key}&units=metric&lang=fr`)
+        .then(function(response){
+          return response.json();
+        }).then(function(response){
+          setVille(response.name);
+          setTemperature(response.main.temp)
+          setDescription(response.weather[0].description)
+          //setIcone(response.weather[0].icon + ".png")
+    })
   }
+  let text = 'Application en cours, veuillez patienter';
+
+  useEffect( () => {
+    location && MeteoVille();
+  })
 
   return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>{text}</Text>
+        <Text style={styles.paragraph}>{ville}</Text>
+        <Text style={styles.paragraph}>{temperature}</Text>
+        <Text style={styles.paragraph}>{description}</Text>
+
       </View>
   );
 }
